9d3750a7d5c998d0d7349b4c62ae3924c24b7b23
manuelMarkDenver <manuel.markdenver@gmail.com>
2022-11-08 10:10:42 +0800

    updates 2

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,28 +2,41 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const Homepage = () => {
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const fetchedPosts = await axios.get(
           `https://crm.jackieconnolly.info/api/wp-json/wp/v2/posts`
-        )
-        setPosts(fetchedPosts.data)
-        
+        );
+
+        setPosts(fetchedPosts.data);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-      
     };
 
     fetchData();
   }, []);
 
-  console.log(posts)
+  console.log(posts);
 
-  return <div>crm</div>;
+  return (
+    <>
+      <div>Creative Approach CRM</div>
+      <ul style={{ listStyle: "none" }}>
+        {posts?.map((post) => {
+          return (
+            <li key={post.id}>
+              <h1 >{post.title.rendered}</h1>
+              <p dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }} />
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default Homepage;
