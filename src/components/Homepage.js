6a61820c218d6c501729162a422a1a94aaaf500f
manuelMarkDenver <manuel.markdenver@gmail.com>
2022-11-16 05:07:17 +0800

    updated banner and navbar

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,10 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
+import axios from "axios";
+import { Box, CircularProgress } from "@mui/material";
 
 const Homepage = () => {
   const [posts, setPosts] = useState([]);
+  const [homepageElements, setHomepageElements] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -11,6 +13,11 @@ const Homepage = () => {
           `https://crm.jackieconnolly.info/api/wp-json/wp/v2/posts`
         );
 
+        const fetchedHomepageElements = await axios.get(
+          `https://crm.jackieconnolly.info/api/wp-json/wp/v2/pages?slug=homepage`
+        );
+
+        setHomepageElements(fetchedHomepageElements.data);
         setPosts(fetchedPosts.data);
       } catch (error) {
         console.log(error);
@@ -20,21 +27,42 @@ const Homepage = () => {
     fetchData();
   }, []);
 
-  console.log(posts);
-
   return (
     <>
-      <div>Creative Approach CRM</div>
-      <ul style={{ listStyle: "none" }}>
-        {posts?.map((post) => {
-          return (
-            <li key={post.id}>
-              <h1 >{post.title.rendered}</h1>
-              <p dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }} />
-            </li>
-          );
-        })}
-      </ul>
+      <Box sx={{ position: "relative" }}>
+        {homepageElements?.[0]?.acf?.banner ? (
+          <Box
+            component="img"
+            alt="background-image"
+            src={homepageElements?.[0]?.acf?.banner}
+            sx={{ width: "100vw" }}
+          />
+        ) : (
+          <Box sx={{
+            height: "100vh",
+            width: "100vw",
+            display: "flex",
+            justifyContent: "center",
+            alignItem: "center"
+          }}>
+            <CircularProgress color="primary"/>
+          </Box>
+        )}
+
+        <Box
+          sx={{
+            position: "absolute",
+            top: "0",
+            bottom: "0",
+            left: "0",
+            right: "0",
+            height: "100%",
+            weight: "100%",
+            backgroundColor: "#000",
+            opacity: "70%",
+          }}
+        ></Box>
+      </Box>
     </>
   );
 };
